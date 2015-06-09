3e581179352648304f4a829b5437dc299922b860
julievdh <julievdh@mit.edu>
2015-06-09 11:31:21 -0400

    Adjusted timing of low/high drag/buoyancy

--- DepthDist.m
+++ DepthDist.m
@@ -6,8 +6,8 @@ load_rw015a
 
 figure(1); clf; set(gcf,'position',[320 360 1020 420])
 subplot('position',[0.28 0.55 0.7 0.4]); hold on
-plot(rw015a.t(rw015a.p1(1)*fs:rw015a.p1(2)*fs),-rw015a.p(rw015a.p1(1)*fs:rw015a.p1(2)*fs),'k'); box on
-plot(rw015a.t(rw015a.p2(1)*fs:rw015a.p3(2)*fs),-rw015a.p(rw015a.p2(1)*fs:rw015a.p3(2)*fs),'b'); box on
+plot(rw015a.t(rw015a.p1(1)*fs:rw015a.p1(2)*fs),-rw015a.p(rw015a.p1(1)*fs:rw015a.p1(2)*fs),'b'); box on
+plot(rw015a.t(rw015a.p2(1)*fs:rw015a.p3(2)*fs),-rw015a.p(rw015a.p2(1)*fs:rw015a.p3(2)*fs),'k'); box on
 
 % plot info/axis management
 ylim([-22 1]); xlim([0 22532])
@@ -24,13 +24,14 @@ subplot('position',[0.05 0.55 0.2 0.4])
 h = barh(b,[h1;h2]','grouped');
 axis ij; ylim([-1 22]); ylabel('Depth (m)')
 set(gca,'ytick',[0:5:30])
-set(h(1),'FaceColor','k')
-set(h(2),'FaceColor','b')
+set(h(1),'FaceColor','b')
+set(h(2),'FaceColor','k')
 
 %% Eg 4057
 subplot('position',[0.28 0.1 0.7 0.4]); hold on
+fs = 5; % for eg047a tag
 plot(eg047a.t(eg047a.p1(1)*fs:10610*fs),-eg047a.p(eg047a.p1(1)*fs:10610*fs),'k'); box on
-plot(eg047a.t(10610*fs:eg047a.p3(2)*fs),-eg047a.p(10610*fs:eg047a.p3(2)*fs),'r'); box on
+plot(eg047a.t(8571*fs:eg047a.p3(2)*fs),-eg047a.p(8571*fs:eg047a.p3(2)*fs),'r'); box on
 
 % plot info/axis management
 ylim([-31 1]); xlim([eg047a.p1(1) 13364])
@@ -41,7 +42,7 @@ xlabel('Time since tag on (hours)')
 % calculate depth distribution
 % while entangled
 [h3,b] = hist(eg047a.p(eg047a.p1(1)*fs:10610*fs),[0:2:31]);
-[h4,b] = hist(eg047a.p(10610*fs:eg047a.p3(2)*fs),[0:2:31]);
+[h4,b] = hist(eg047a.p(8571*fs:eg047a.p3(2)*fs),[0:2:31]);
 h3 = h3/sum(h3);
 h4 = h4/sum(h4);
 subplot('position',[0.05 0.1 0.2 0.4])
