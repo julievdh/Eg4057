51a3ef9ac5d3418b0157f062128a6b8dfcd460c2
julievdh <julievdh@mit.edu>
2015-06-15 11:49:16 -0400

    FS expectation
    What do we expect to see with specific shapes? Then, what do we actually
    see (with FS_descasc)

---
+++ FS_descasc.m
@@ -0,0 +1,131 @@
+close all
+
+for dive = 1:length(rw015a.T)
+figure(1); hold on
+
+plot(-rw015a.p(rw015a.T(dive,1)*fs:rw015a.T(dive,2)*fs)) % plot dive
+plot(end_desc(dive),-rw015a.p(rw015a.T(dive,1)*fs+end_desc(dive)),'k*')
+plot(start_asc(dive),-rw015a.p(rw015a.T(dive,1)*fs+start_asc(dive)),'k*')
+plot(rw015a.ph(rw015a.T(dive,1)*fs:rw015a.T(dive,2)*fs)) % plot pitch during dive
+
+% plot descent and ascent only
+plot(-rw015a.p(rw015a.T(dive,1)*fs:rw015a.T(dive,1)*fs+end_desc(dive)),'r')
+plot(-rw015a.p(rw015a.T(dive,1)*fs+start_asc(dive):rw015a.T(dive,2)*fs),'r')
+
+% find maxtab within descent and ascent
+fs_desc = find(maxtab(:,1) > rw015a.T(dive,1)*fs & maxtab(:,1) <rw015a.T(dive,1)*fs+end_desc(dive));
+fs_asc = find(maxtab(:,1) > rw015a.T(dive,1)*fs+start_asc(dive) & maxtab(:,1) < rw015a.T(dive,2)*fs);
+
+ii = fs_desc;
+
+ud_diff = nan(length(maxtab(ii)),50);
+for i = 1:2:length(maxtab(ii)); % every second one
+    %%
+    % % plot full stroke
+    %     patchline(maxtab(ii(i),1):maxtab(ii(i+1),1),rw015a.ph(maxtab(ii(i,1)):maxtab(ii(i+1,1))),'edgecolor','k','edgealpha',0.1,'LineWidth',3)
+    %     % plot half stroke down
+    %     patchline(maxtab(ii(i),1):mintab(ii(i),1),rw015a.ph(maxtab(ii(i),1):mintab(ii(i),1)),'edgecolor','b','edgealpha',0.5)
+    %     % plot half stroke up
+    %     patchline(mintab(ii(i),1):maxtab(ii(i+1),1),rw015a.ph(mintab(ii(i),1):maxtab(ii(i+1),1)),'edgecolor','r','edgealpha',0.5)
+    %
+    % plot half stroke return to zero
+    % how long does x vector need to be?
+    need = maxtab(ii(i)+1,1)-mintab(ii(i),1);
+    xvec = mintab(ii(i),1):-1:mintab(ii(i),1)-need;
+    %  patchline(xvec,rw015a.ph(mintab(ii(i),1):maxtab(ii(i+1),1)),'edgecolor','r','edgealpha',0.5)
+    
+    % plot normalized
+    figure(12); set(gcf,'position',[24 132 1244 500])
+    subplot(221); title('Descent')
+    patchline([maxtab(ii(i),1):maxtab(ii(i)+1,1)]-mintab(ii(i),1),rw015a.ph(maxtab(ii(i,1)):maxtab(ii(i)+1,1)),'edgecolor','k','edgealpha',0.1,'LineWidth',3)
+    % plot half stroke down
+    patchline([maxtab(ii(i),1):mintab(ii(i),1)]-mintab(ii(i),1),rw015a.ph(maxtab(ii(i),1):mintab(ii(i),1)),'edgecolor','b','edgealpha',0.5)
+    % plot half stroke up
+    patchline([mintab(ii(i),1):maxtab(ii(i)+1,1)]-mintab(ii(i),1),rw015a.ph(mintab(ii(i),1):maxtab(ii(i)+1,1)),'edgecolor','r','edgealpha',0.5)
+      
+    down = rw015a.ph(maxtab(ii(i),1):mintab(ii(i),1)); % downstroke
+    up = rw015a.ph(mintab(ii(i),1):mintab(ii(i),1)+need+1); % upstroke
+    
+    ylim([-1 1])
+    
+    % set colour based on high/low drag
+    if dive <= 17
+        col = [0 0 1];
+    else col = [0 0 0];
+    end
+    
+    % make down and up the same lengths
+    if length(down) > length(up)
+        down_short = down(length(down)-length(up)+1:end);
+        ud_diff(i,1:length(up)) = (down_short - flip(up))';
+        figure(12); subplot(222); hold on; patchline(down_short,flip(up),'edgecolor',col,'edgealpha',0.1)
+    else if length(up) > length(down)
+            up_short = up(1:length(up)-(length(up)-length(down)));
+            ud_diff(i,1:length(up_short)) = (down - flip(up_short))';
+            figure(12); subplot(222); hold on; patchline(down,flip(up_short),'edgecolor',col,'edgealpha',0.1)
+        else
+            ud_diff(i,1:length(up)) = (down - flip(up))';
+            figure(12); subplot(222); hold on; patchline(down,flip(up),'edgecolor',col,'edgealpha',0.1)
+        end
+    end
+    xlabel('Downstroke Deviation'); ylabel('Upstroke Deviation')
+    xlim([-0.5 0.5]); ylim([-0.5 0.5]); axis equal
+    plot([-1 1],[-1 1],'k')
+    
+    pause; clf
+end
+ 
+
+%% 
+ii = fs_asc;
+
+ud_diff = nan(length(maxtab(ii)),50);
+for i = 1:length(maxtab(ii));
+    % % plot full stroke
+    %     patchline(maxtab(ii(i),1):maxtab(ii(i+1),1),rw015a.ph(maxtab(ii(i,1)):maxtab(ii(i+1,1))),'edgecolor','k','edgealpha',0.1,'LineWidth',3)
+    %     % plot half stroke down
+    %     patchline(maxtab(ii(i),1):mintab(ii(i),1),rw015a.ph(maxtab(ii(i),1):mintab(ii(i),1)),'edgecolor','b','edgealpha',0.5)
+    %     % plot half stroke up
+    %     patchline(mintab(ii(i),1):maxtab(ii(i+1),1),rw015a.ph(mintab(ii(i),1):maxtab(ii(i+1),1)),'edgecolor','r','edgealpha',0.5)
+    %
+    % plot half stroke return to zero
+    % how long does x vecto need to be?
+    need = maxtab(ii(i)+1,1)-mintab(ii(i),1);
+    xvec = mintab(ii(i),1):-1:mintab(ii(i),1)-need;
+    %  patchline(xvec,rw015a.ph(mintab(ii(i),1):maxtab(ii(i+1),1)),'edgecolor','r','edgealpha',0.5)
+    
+    % plot normalized
+    figure(12); set(gcf,'position',[24 132 1244 500])
+    subplot(223); title('Ascent')
+    patchline([maxtab(ii(i),1):maxtab(ii(i)+1,1)]-mintab(ii(i),1),rw015a.ph(maxtab(ii(i,1)):maxtab(ii(i)+1,1)),'edgecolor','k','edgealpha',0.1,'LineWidth',3)
+    % plot half stroke down
+    patchline([maxtab(ii(i),1):mintab(ii(i),1)]-mintab(ii(i),1),rw015a.ph(maxtab(ii(i),1):mintab(ii(i),1)),'edgecolor','b','edgealpha',0.5)
+    % plot half stroke up
+    patchline([mintab(ii(i),1):maxtab(ii(i)+1,1)]-mintab(ii(i),1),rw015a.ph(mintab(ii(i),1):maxtab(ii(i)+1),1),'edgecolor','r','edgealpha',0.5)
+    
+    ylim([-1 1])
+    
+    down = rw015a.ph(maxtab(ii(i),1):mintab(ii(i),1)); % downstroke
+    up = rw015a.ph(mintab(ii(i),1):mintab(ii(i),1)+need+1); % upstroke
+    % make down and up the same lengths
+    if length(down) > length(up)
+        down_short = down(length(down)-length(up)+1:end);
+        ud_diff(i,1:length(up)) = (down_short - flip(up))';
+        figure(12); subplot(224); hold on; patchline(down_short,flip(up),'edgecolor',col,'edgealpha',0.1)
+    else if length(up) > length(down)
+            up_short = up(1:length(up)-(length(up)-length(down)));
+            ud_diff(i,1:length(up_short)) = (down - flip(up_short))';
+            figure(12); subplot(224); hold on; patchline(down,flip(up_short),'edgecolor',col,'edgealpha',0.1)
+        else
+            ud_diff(i,1:length(up)) = (down - flip(up))';
+            figure(12); subplot(224); hold on; patchline(down,flip(up),'edgecolor',col,'edgealpha',0.1)
+        end
+    end
+    xlabel('Downstroke Deviation'); ylabel('Upstroke Deviation')
+    xlim([-0.5 0.5]); ylim([-0.5 0.5]); axis equal
+    plot([-1 1],[-1 1],'k')  
+    
+    pause; clf
+end
+
+end
---
+++ FS_expectation.m
@@ -0,0 +1,140 @@
+% STROKE EXPECTATION
+
+% what do different strokes look like and what do we expect? 
+
+%% equal upstroke and downstroke
+% make data
+downx = 0:12; 
+downy = [0.101816459079515;0.101545443005913;0.0908208264168652;...
+    0.0747919440142817;0.0538283411710264;0.0315051320492651;...
+    0.0104699903883109;-0.0117576100318742;-0.0353408616048715;...
+    -0.0598103073506884;-0.0826227967240054;-0.0951138511625120;...
+    -0.0960095136452938];
+upx = 12:-1:0;
+upy = flipud(downy);
+
+% plot
+figure(2); set(gcf,'position',[71 226 978 394]); clf
+% THROUGH TIME
+subplot(131); hold on
+plot(downx/fs,downy,'b')
+plot((downx+12)/fs,upy,'r')
+xlabel('Time (sec)'); ylabel('Pitch Deviation (deg)')
+
+% OVERLAYED
+subplot(132); hold on
+patchline((downx-downx(end))/fs,downy,'edgecolor','b','edgealpha',0.5)
+patchline((upx-upx(1))/fs,upy,'edgecolor','r','edgealpha',0.5)
+xlabel('Time (sec)'); ylabel('Pitch Deviation (deg)')
+
+% DEVIATION
+subplot(133); hold on
+plot(downy,flipud(upy),'k')
+xlim([-.2 .2]); ylim([-.2 .2])
+xlabel('Downstroke Position'); ylabel('Upstroke Position')
+
+% %% same shape
+% downx = 0:9; 
+% downy = [0.163847440028824;0.161807463535088;0.138941364011919;...
+%     0.0967763322761938;0.0475187774730079;0.000526690204396586;...
+%     -0.0377117417063299;-0.0665332694647288;-0.0902697506550811;...
+%     -0.104351673730929];
+% upx = 9:-1:-1;
+% upy = [-0.104351673730929;-0.101060310872823;-0.0863962703644158;-0.0658234476682039;-0.0348994915693063;0.00576349924835434;0.0514811182717512;0.0987725691683427;0.139185147053790;0.165397937025817;0.174747121696217];
+% 
+% % THROUGH TIME
+% figure(2); 
+% subplot(131); hold on
+% plot(downx,downy,'b')
+% plot(9:19,upy,'r')
+% 
+% % OVERLAYED
+% subplot(132); hold on
+% plot(downx,downy,'b')
+% plot(upx,upy,'r')
+% 
+% % DEVIATION
+% subplot(133); hold on
+% plot(downy(1:length(downx)),upy(1:length(downx)),'k')
+% xlim([-.2 .2]); ylim([-.2 .2])
+
+%% downstroke stronger than upstroke
+downx = 0:7;
+downy = [0.0813774986220460;0.0721532623085626;0.0465551326638748;0.0132201561395104;-0.0190445078281062;-0.0482797431347104;-0.0719155934421165;-0.0819364476636020];
+upx = 7:-1:-7;
+upy = [-0.0820906432748538;-0.0806286549707602;-0.0762426900584795;...
+    -0.0703947368421053;-0.0645467836257310;-0.0499269005847953;...
+    -0.0345760233918129;-0.0133771929824561;0.0114766081871345;...
+    0.0399853801169591;0.0531432748538012;0.0633771929824561;...
+    0.0687; 0.0734; 0.0804];
+
+% THROUGH TIME
+subplot(131)
+plot((downx)/fs,downy,'b--')
+plot((7:21)/fs,upy,'r--')
+
+% OVERLAYED
+subplot(132)
+plot((downx-downx(end))/fs,downy,'b--')
+plot((upx-upx(1))/fs,upy,'r--')
+
+% DEVIATION
+subplot(133)
+plot(downy(1:length(downx)),flipud(upy(1:length(downx))),'k--')
+xlim([-.2 .2]); ylim([-.2 .2])
+
+
+
+% %% upstroke stronger than downstroke
+% downx = 0:16;
+% downy = [0.0753420258071424;0.0743025017948203;0.0672110653156623;0.0585088407393991;0.0562665589540546;0.0506894832676073;0.0352596492853526;0.0169542016219108;0.00284566976757769;-0.00477379236355504;-0.0143954812444353;-0.0305287850961264;-0.0461209340248310;-0.0613458925500460;-0.0867357578483866;-0.112181164594956;-0.112638191094222];
+% upx = 16:-1:8;
+% upy = [-0.112638191094222;-0.0981417976342911;-0.0749437935402308;-0.0170912571991557;0.0861084858527020;0.257358250158189;0.453882798051257;0.540492227078605;0.501850634475790];
+% 
+% % THROUGH TIME
+% subplot(131)
+% plot(downx,downy,'b:')
+% plot(16:16+8,upy,'r:')
+% 
+% % OVERLAYED
+% subplot(132)
+% plot(downx,downy,'b:')
+% plot(upx,upy,'r:')
+% 
+% % DEVIATION
+% subplot(133); 
+% plot(downy(1:length(upx)),flipud(upy),'k:')
+% xlim([-.2 .4]); ylim([-.2 .4])
+
+
+%% try one more (should be ABOVE the line)
+downx = (0:14)/fs;
+downy = [0.0512128172171286;0.0474636068531109;0.0419004329315816;0.0338107290045169;0.0223761850775371;0.00851648688283585;-0.00488359492574893;-0.0139284363261507;-0.0209467757434129;-0.0302817055950170;-0.0405487598749185;-0.0476566320979767;-0.0523110441728800;-0.0577938798393188;-0.0612754205098862];
+upx = (14:-1:0)/fs;
+upy = [-0.0612754205098862;-0.0596423967013243;-0.0553719184443976;-0.0491259690622049;-0.0373978937522058;-0.0192652893001632;0.00244060828502576;0.0247558668000754;0.0450283622561640;0.0616848601308887;0.0756386420620984;0.0867532400847642;0.0926316542199352;0.0926481638650162;0.0853454615506130];
+
+% THROUGH TIME
+subplot(131)
+plot(downx/fs,downy,'b:')
+plot((16:16+14)/fs,upy,'r:')
+
+text(0.42,0.13,'A','FontWeight','bold','FontSize',20)
+
+% OVERLAYED
+subplot(132)
+plot((downx-downx(end))/fs,downy,'b:')
+plot((upx-upx(1))/fs,upy,'r:')
+
+text(-2.79,0.13,'B','FontWeight','bold','FontSize',20)
+
+% DEVIATION
+subplot(133); 
+plot(downy(1:length(upx)),flipud(upy),'k:')
+xlim([-.1 .1]); ylim([-.1 .15])
+
+text(-0.085,0.13,'C','FontWeight','bold','FontSize',20)
+
+adjustfigurefont
+
+cd C:\Users\Julie\Documents\MATLAB\Eg4057\AnalysisFigs
+print('FSexpectation','-dtiff')
